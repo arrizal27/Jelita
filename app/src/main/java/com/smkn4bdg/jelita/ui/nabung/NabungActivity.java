0865077a590a68171f20366277e7f0b6f2353681
Bama Qyandija Deandra <bamaqyan@gmail.com>
2021-03-25 19:16:42 +0700

    nabung
    yang error di control z ku urg ehe

--- app/src/main/java/com/smkn4bdg/jelita/ui/nabung/NabungActivity.java
+++ app/src/main/java/com/smkn4bdg/jelita/ui/nabung/NabungActivity.java
@@ -1,20 +1,37 @@
 package com.smkn4bdg.jelita.ui.nabung;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.smkn4bdg.jelita.R;
 import com.smkn4bdg.jelita.ui.setor.SetorActivity;
 
 public class NabungActivity extends AppCompatActivity {
+    private DatabaseReference mdbUsers;
+    private FirebaseDatabase mfirebaseInstance;
+    private FirebaseAuth mfirebaseauth;
+    private FirebaseUser mUser;
+    private final String TAG = this.getClass().getName().toUpperCase();
     MaterialButton btnBack, btnTabung, btnSetor;
+    TextView tabunganMinyak, kapasitasMax;
     TextInputEditText txtJumlahMinyak;
     ProgressBar progressBarMinyak;
 
@@ -22,7 +39,7 @@ public class NabungActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nabung);
-
+        initdata();
         findView();
 
         btnBack.setOnClickListener(new View.OnClickListener() {
@@ -49,6 +66,47 @@ public class NabungActivity extends AppCompatActivity {
             }
         });
     }
+    private void initdata(){
+        //for read. and crud
+        // TODO: 25/03/2021 lanjutin crud tabungan minyak antisisapi error 
+        FirebaseApp.initializeApp(this);
+        mfirebaseInstance = FirebaseDatabase.getInstance();
+        mUser = FirebaseAuth.getInstance().getCurrentUser();
+        mdbUsers = mfirebaseInstance.getReference();
+        mdbUsers.child("users").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot mdata : snapshot.getChildren()){
+                    if (mdata.child("id").getValue().equals(mUser.getUid())){
+                        tabunganMinyak.setText(String.valueOf(mdata.child("jml_minyak").getValue() + " Liter"));
+                        progressBarMinyak.setProgress(Integer.valueOf(mdata.child("jml_minyak").getValue().toString()));
+                        if(mdata.child("role").getValue().toString().equals("Rumah Tangga")){
+                            kapasitasMax.setText(Integer.valueOf(5) + " Liter");
+                            progressBarMinyak.setMax(5);
+                        }
+                        if (mdata.child("role").getValue().toString().equals("Pedagang")){
+                            kapasitasMax.setText(Integer.valueOf(10) + " Liter");
+                            progressBarMinyak.setMax(10);
+                        }
+                        if (mdata.child("role").getValue().toString().equals("Cafe dan Rumah Makan")){
+                            kapasitasMax.setText(Integer.valueOf(15) + " Liter");
+                            progressBarMinyak.setMax(15);
+                        }
+                        if (mdata.child("role").getValue().toString().equals("Hotel dan Penginapan")){
+                            kapasitasMax.setText(Integer.valueOf(20) + " Liter");
+                            progressBarMinyak.setMax(20);
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+    }
 
     private void findView(){
         btnBack = findViewById(R.id.btn_back);
@@ -56,6 +114,8 @@ public class NabungActivity extends AppCompatActivity {
         btnSetor = findViewById(R.id.btn_setor);
         txtJumlahMinyak = findViewById(R.id.txt_tambah_minyak);
         progressBarMinyak = findViewById(R.id.progres_nabung);
+        tabunganMinyak = findViewById(R.id.txt_tabungan);
+        kapasitasMax = findViewById(R.id.txt_maks);
     }
 
     private void nabungSuccess() {
